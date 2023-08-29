0fac3af69ba3014d655cf4ab6cb8cddac1df4360
Charlie Greenman <charlie@razroo.com>
2023-08-29 12:38:01 -0700

    ZETA-6590: Wire up unit tests for library.

--- src/rz/angular/effects/library/library.spec.ts
+++ src/rz/angular/effects/library/library.spec.ts
@@ -1,8 +1,49 @@
+import { standaloneEffects } from "../../../morph";
+import { EditFileEffect } from "../../../morph/interfaces/morph.interface";
+import { AngularTypeNames } from "../../types/types";
 import { returnRootTsConfig } from "./library";
 
 describe('Library', () => {
-  it('should return the root tsconfig', () => {
-    const result = returnRootTsConfig('', [], []);
-    expect(result).toEqual(['tsconfig.base.json']);
-  });  
+  describe('returnRootTsConfig', () => {
+    it('should return the root tsconfig to modify as well as package json to get project name from', () => {
+      const result = returnRootTsConfig('', [], []);
+      expect(result).toEqual(['tsconfig.base.json', 'package.json']);
+    });  
+  });
+
+  describe('libraryEffects', () => {
+    it('should modify the base tsconfig with the appropriate params', () => {
+      const programmingLanguage = 'angular';
+      const mockParameter = {
+        type: AngularTypeNames.Library
+      } as any;
+      const mockPackageJson = `{
+        "name": "test-codegen-eleven",
+        "version": "0.0.0",
+      }`;
+      const mockTsConfigBase = `{
+        "compileOnSave": false,
+        "compilerOptions": {
+          "paths": {
+            "@test-codegen-eleven/common/common-ui": ["libs/common/common-ui/src/index.ts"],
+          }   
+        }
+      `;
+      const mockFileEffects: EditFileEffect[] = [
+        {
+          filePath: 'tsconfig.base.json',
+          originFilePath: 'path/to/another/src/hello.component.ts',
+          content: mockTsConfigBase
+        },
+        {
+          filePath: 'package.json',
+          originFilePath: 'path/to/another/src/hello.component.ts',
+          content: mockPackageJson
+        },
+      ];
+      const result = standaloneEffects(programmingLanguage, mockParameter, mockFileEffects);
+
+      expect(result).toEqual([]);
+    });
+  });
 });
--- src/rz/angular/effects/library/library.ts
+++ src/rz/angular/effects/library/library.ts
@@ -1,3 +1,36 @@
+import { morphJson } from "../../../json/json-morph";
+import { EditFileEffect } from "../../../morph/interfaces/morph.interface";
+
+// returns package json as well, so can get package json data
 export function returnRootTsConfig(filePathWithName: string, fileTree: string[], optionalTypes: AngularOptionalType[]): string[] {
-  return ['tsconfig.base.json'];  
+  return ['tsconfig.base.json', 'package.json'];  
+}
+
+export function libraryEffects(fileEffects: EditFileEffect[]): EditFileEffect[] {
+    for(const fileEffect of fileEffects) {
+      const filePath = fileEffect.filePath;
+      const originFilePath = fileEffect.originFilePath;
+      if(filePath.includes('tsconfig.base.json')) {
+        // get project name
+        const packageJson = fileEffects.find(fileEffect => fileEffect.filePath === 'package.json')
+        const packageJsonParse = packageJson && JSON.parse(packageJson.content);
+        const projectName = packageJsonParse.name;
+        const fileName = filePath.split('/').pop();
+        const fileToBeAddedTo = fileEffect.content;
+        
+        const editInput: any = {
+            fileType: 'ts',
+            fileName: fileName,
+            filePath: filePath,
+            fileToBeAddedTo: fileToBeAddedTo,
+            edits: [{
+              nodeType: 'addJsonKeyValue',
+              valueToModify: '/compilerOptions/paths',
+              codeBlock: {["@" + projectName + "/home"]: ["libs/home/src/index.ts"]}
+            }]
+        };
+        const updatedFileToBeAddedTo = morphJson(editInput);
+        fileEffect.content = updatedFileToBeAddedTo;
+      }
+    }
 }
