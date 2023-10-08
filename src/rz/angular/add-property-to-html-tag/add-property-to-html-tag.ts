e85dd1647d0270114890659d86a570323ecb45f7
Charlie Greenman <charlie@razroo.com>
2023-10-07 21:02:31 -0400

    add fix for stringified json

--- src/rz/angular/add-property-to-html-tag/add-property-to-html-tag.ts
+++ src/rz/angular/add-property-to-html-tag/add-property-to-html-tag.ts
@@ -5,12 +5,13 @@ import { EditHtmlFile } from '../interfaces/edit-html.interface';
 
 // will insert code into an html block
 export function addPropertyToHtmlTag(editFile: EditHtmlFile, fileToBeModified: any): any {
+  const codeBlock = typeof editFile.codeBlock === 'string' ? JSON.parse(editFile.codeBlock) : editFile.codeBlock;
   visit(fileToBeModified, { type: 'element', tagName: editFile.tagNameToInsertInto }, (node: any, index) => {
     // Check if the tag is the one you want to modify
     if (node.tagName === editFile.tagNameToInsertInto) {
       node.properties = {
         ...(node.properties || {}),
-        ...editFile.codeBlock
+        ...codeBlock
       };
     }
   });
