ea10b5d3de2de9de6d09cfb025a15f805865e09d
Charlie Greenman <charlie@razroo.com>
2023-10-08 12:49:12 -0400

    ZETA-6890: Add initial unit test.

---
+++ src/rz/angular/insert-into-html-tag/insert-into-html-tag.spec.ts
@@ -0,0 +1,32 @@
+import { morphCode } from "../../morph";
+import { EditHtmlFile, EditHtmlInput } from "../interfaces/edit-html.interface";
+
+describe('insertIntoHtmlTag', () => {
+    it('should insert html into the specified tag', () => {
+      const fileToBeAddedTo = `<div class="wrapper">
+<div class="container">Your component goes here</div>
+</div>
+`;
+      const codeBlock = '<div>new div content</div>';
+      const insertIntoHtmlTag: EditHtmlFile = {
+        nodeType: 'insertIntoHtmlTag',
+        codeBlock: codeBlock,
+        tagNameToInsertInto: 'div'
+      };
+
+      const editHtmlInput: EditHtmlInput = {
+        fileType: 'html',
+        fileToBeAddedTo: fileToBeAddedTo,
+        edits: [
+          insertIntoHtmlTag
+        ]
+      };
+
+      const expected = `<div class="wrapper">
+<div class="container">Your component goes here <div>new div content</div></div>
+</div>
+`;
+      const newHtmlString = morphCode(editHtmlInput);
+      expect(newHtmlString).toEqual(expected);
+    });
+  });
