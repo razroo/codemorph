0b7b97196bc96b0329b4ffa877dedf10acc4528e
Charlie Greenman <charlie@razroo.com>
2023-10-18 22:01:30 -0400

    ZETA-6925: Update codemorph to include module as well.

--- src/rz/angular/effects/module/module.spec.ts
+++ src/rz/angular/effects/module/module.spec.ts
@@ -1,4 +1,5 @@
-import { filesToAffect } from "../../../morph";
+import { filesToAffect, standaloneEffects } from "../../../morph";
+import { EditFileEffect } from "../../../morph/interfaces/morph.interface";
 import { AngularTypeNames } from "../../types/types";
 
 describe('AngularModule', () => {
@@ -17,5 +18,28 @@ describe('AngularModule', () => {
     ];
     const fileToModify = filesToAffect(mockFilePath, fileTree, mockParameter, 'angular');
     expect(fileToModify).toEqual(['path/to/another/index.ts']);
+  });
+
+  it('should morph index file and add index ts file', () => {
+    const programmingLanguage = 'angular';
+    const mockParameter = {
+      type: AngularTypeNames.Module
+    } as any;
+    const mockFileEffects: EditFileEffect[] = [{
+      filePath: 'path/to/another/index.ts',
+      originFilePath: 'path/to/another/src/hello.component.ts',
+      content: `import { NgModule } from '@angular/core';
+      `
+    }];
+    const result = standaloneEffects(programmingLanguage, mockParameter, mockFileEffects);
+    const expectedIndexContent = `import { NgModule } from '@angular/core';
+
+export * from "./src/hello.component";
+`;
+    expect(result).toEqual([{
+      content: expectedIndexContent,
+      filePath: 'path/to/another/index.ts',
+      originFilePath: 'path/to/another/src/hello.component.ts',
+    }]);
   });
 });
