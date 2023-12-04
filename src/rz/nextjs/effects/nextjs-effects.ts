2aaec34b94e25cefa9570222cf2e71a397f658f0
Charlie Greenman <charlie@razroo.com>
2023-12-04 18:47:40 -0500

    ZETA-7230: Update nextjs effects for the appliation

--- src/rz/nextjs/effects/nextjs-effects.ts
+++ src/rz/nextjs/effects/nextjs-effects.ts
@@ -1,8 +1,11 @@
 import { AngularOptionalType } from "../../angular/types/types";
 import { NOT_SUPPORTED_TYPE } from "../../morph";
-import { NextjsTypeNames } from "../types/nextjs-types";
+import { NextjsOptionalType, NextjsTypeNames } from "../types/nextjs-types";
+import { returnRootTsConfig } from "./library/nextjs-library";
 
-export function angularFilesToAffect(filePathWithName: string, fileTree: string[], type: NextjsTypeNames, optionalTypes: AngularOptionalType[]): string[] | NOT_SUPPORTED_TYPE {
+export function nextjsFilesToAffect(filePathWithName: string, fileTree: string[], type: NextjsTypeNames, optionalTypes: NextjsOptionalType[]): string[] | NOT_SUPPORTED_TYPE {
     switch(type) {
+      case NextjsTypeNames.Library:
+        return returnRootTsConfig(filePathWithName, fileTree, optionalTypes);
     }
 }
