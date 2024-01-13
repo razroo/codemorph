9b459543368e36900ab96f02831f18c28406e4b1
Charlie Greenman <charlie@razroo.com>
2024-01-13 18:20:12 -0500

    ZETA-7642: Add logic for adding json object even as a string for add json logic

---
+++ src/rz/json/add-json/add-json.ts
@@ -0,0 +1,21 @@
+import { JSONPath } from 'jsonpath-plus';
+import { EditJson } from "../interfaces/json-morph.interface";
+import * as pointer from 'json-pointer';
+
+export function addJsonKeyValue(editJson: EditJson, json: string): any {
+    // double json parse hack to make sure /n are removed from string
+    const codeBlock = typeof editJson.codeBlock === 'string' ? JSON.parse(JSON.parse(JSON.stringify(editJson.codeBlock))) : editJson.codeBlock;
+    json = JSON.parse(json);
+    //Get Pointer
+    const JsonPointer = JSONPath({path: `$..${editJson.valueToModify}`, json, resultType: 'pointer'});
+    const firstJsonMatchedPointer = JsonPointer[0];
+  
+    //Get value of json
+    const jsonToEdit = pointer.get(json as any, firstJsonMatchedPointer);
+  
+    //Set value
+    pointer.set(json as any, firstJsonMatchedPointer, {...jsonToEdit, ...codeBlock});
+   
+    //Return modified value
+    return json;
+  }
--- src/rz/json/edit-json/edit-json.spec.ts
+++ src/rz/json/edit-json/edit-json.spec.ts
@@ -1,6 +1,6 @@
-import { readFileSync } from 'fs';
 import { EditJson } from './../interfaces/json-morph.interface';
-import { editJson, addJsonKeyValue } from './edit-json';
+import { editJson } from './edit-json';
+
 describe('editJson', () => {
   it('should modify nested json and return a string value', () => {
     const mockJson = {
@@ -28,29 +28,27 @@ describe('editJson', () => {
   });
 
   it('should edit nested json and return value', () => {
-    const mockJson = readFileSync('src/rz/json/snapshots/nested-package-json/nested-package.json.snap').toString();
+    const mockJson = {
+      contributes: {
+        menus: {}
+      }
+    };
+
+    const stringifiedMockJson = JSON.stringify(mockJson);
   
     const mockEditJson: EditJson = {
       nodeType: 'editJson',
       valueToModify: '/contributes/menus',
       codeBlock: {data: "test"}
     }
-    const modifiedJson = editJson(mockEditJson, mockJson);
-    const expected = JSON.parse(readFileSync('src/rz/json/snapshots/nested-package-json/nested-package-output.json.snap').toString());
-
-    expect(modifiedJson).toEqual(expected);
-  });
-
-  it('should add a key value', () => {
-    const mockJson = readFileSync('src/rz/json/snapshots/package-json/package.json.snap').toString();
-    const mockEditJson: EditJson = {
-      nodeType: 'editJson',
-      valueToModify: 'scripts',
-      codeBlock: '{"test:ci": "npm run nx -- run-many --target=test --all --parallel --coverage --coverageReporters=lcov && node ./tools/coverageMerger.js"}'
-    }
-
-    const modifiedJson = addJsonKeyValue(mockEditJson, mockJson);
-    const expected = JSON.parse(readFileSync('src/rz/json/snapshots/package-json/package-output.json.snap').toString());
+    const modifiedJson = editJson(mockEditJson, stringifiedMockJson);
+    const expected = {
+      contributes: {
+        menus: {
+          data: 'test'
+        }
+      }
+    };
 
     expect(modifiedJson).toEqual(expected);
   });
